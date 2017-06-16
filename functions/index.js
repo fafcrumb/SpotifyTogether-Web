af9efa5e1795527fc1492295cd081c03de69927e
fafcrumb <bri.ferch@gmail.com>
2017-06-16 02:54:00 -0400

    one more messy commit

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,25 @@
-var functions = require('firebase-functions');
+const functions = require('firebase-functions');
+const rp = require('request-promise-native');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// })
+exports.spotifyAuth = functions.https.onRequest((request, response) => {
+  const options = {
+    method: 'POST',
+    uri: 'https://accounts.spotify.com/api/token',
+    form: {
+      grant_type: 'client_credentials',
+    },
+    headers: {
+      authorization: 'Basic MjJjZDVkM2EwMzA0NGZiYzlkOWQwZWM5YjIzZmE2NmI6ZmU1Mzg5MmU3YTgwNDQ3Yjk0M2YwYWE2NmMxOWRkYzA=',
+    },
+  };
+
+  rp(options)
+    .then((parsedBody) => {
+      console.log(parsedBody);
+      response.send(parsedBody);
+    })
+    .catch((err) => {
+      console.error(err.message);
+      response.send(err.message);
+    });
+});
